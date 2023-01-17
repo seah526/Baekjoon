c7cc676072312d30ec4e769b572a6655e601e199
seah526 <50127687+seah526@users.noreply.github.com>
2023-01-17 12:42:37 +0900

    [level 0] Title: 배열의 유사도, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,6 +1,10 @@
 def solution(s1, s2):
-    answer = 0
-    for element in s1 :
-        if element in s2:
-            answer += 1
-    return answer
+    return len([i for i in s1 if i in s2]) 
+    # answer = 0
+    # for element in s1 :
+    #     if element in s2:
+    #         answer += 1
+    # return answer
+
+# def solution(s1, s2):
+#     return len(set(s1)&set(s2));
