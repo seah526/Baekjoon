8da6e1acd4269a18877b0af1a6cbee14063a6a9c
seah526 <50127687+seah526@users.noreply.github.com>
2023-01-17 10:47:34 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
@@ -4,3 +4,9 @@ def solution(n):
         answer.append(n%10)
         n //= 10
     return answer
+
+# def digit_reverse(n):
+#     return [int(i) for i in str(n)][::-1]
+
+# def digit_reverse(n):
+#     return list(map(int, reversed(str(n))))
