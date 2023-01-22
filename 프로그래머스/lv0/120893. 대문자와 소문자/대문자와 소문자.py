e490e63fe2a74e5fdd82f9abb26bee12ece96bf9
seah526 <50127687+seah526@users.noreply.github.com>
2023-01-22 17:07:51 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.14 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    ans = ''
+    for i in my_string :
+        if i.islower() :
+            ans += i.upper()
+        else :
+            ans += i.lower()
+    return ans
