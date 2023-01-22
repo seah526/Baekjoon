baa1e425793e15a3b62a6e480195dfc7f0f9f276
seah526 <50127687+seah526@users.noreply.github.com>
2023-01-22 17:59:18 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.08 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
@@ -1,5 +1,6 @@
 def solution(arr, divisor):
-    ans = sorted([i for i in arr if i%divisor==0]) 
-    if len(ans) < 1 :
-        ans = [-1]
-    return ans
+    print(sorted([i for i in arr if i%divisor==0]))
+    # ans = sorted([i for i in arr if i%divisor==0]) 
+    # if len(ans) < 1 :
+    #     ans = [-1]
+    return sorted([i for i in arr if i%divisor==0]) or [-1]
