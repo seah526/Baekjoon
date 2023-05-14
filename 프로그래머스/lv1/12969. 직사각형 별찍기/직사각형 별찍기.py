bbb87193388bab8e6c74d347ab98c9a7519f54b5
seah526 <50127687+seah526@users.noreply.github.com>
2023-05-14 11:02:31 +0900

    [level 1] Title: 직사각형 별찍기, Time: 11.22 ms, Memory: 7.57 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.py
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.py
@@ -1,5 +1,9 @@
+# a, b = map(int, input().strip().split(' '))
+# for i in range(b):
+#     for k in range(a):
+#         print("*", end="")
+#     print()
+
 a, b = map(int, input().strip().split(' '))
-for i in range(b):
-    for k in range(a):
-        print("*", end="")
-    print()
+answer = ('*'*a +'\n')*b
+print(answer)
