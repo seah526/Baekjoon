c09eeb16ffb84d3173bcdb2f67c9cf1e0815997e
seah526 <50127687+seah526@users.noreply.github.com>
2023-05-15 23:09:19 +0900

    [unrated] Title: 길이에 따른 연산, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.py
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -1,3 +1,5 @@
+from math import prod
+
 def mult(num_list) :
     ans = 1
     for i in num_list :
@@ -5,4 +7,6 @@ def mult(num_list) :
     return ans
 
 def solution(num_list):
-    return sum(num_list) if (len(num_list) >= 11) else mult(num_list)
+    # return sum(num_list) if (len(num_list) >= 11) else mult(num_list)
+
+    return sum(num_list) if (len(num_list)>= 11) else prod(num_list)
