e3de1f7f628f854502eb64bae212a68f8f1c1e62
seah526 <50127687+seah526@users.noreply.github.com>
2023-06-15 13:31:56 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,15 +1,16 @@
 def solution(s):
     
-    answer = ''
-    count = 0
-    for a in s :
-        if a == ' ':
-            count = 0
-            answer += a
-        else :
-            if count % 2 :
-                answer += a.lower()
-            else :
-                answer += a.upper()
-            count += 1
-    return answer
+    # answer = ''
+    # count = 0
+    # for a in s :
+    #     if a == ' ':
+    #         count = 0
+    #         answer += a
+    #     else :
+    #         if count % 2 :
+    #             answer += a.lower()
+    #         else :
+    #             answer += a.upper()
+    #         count += 1
+    # return answer
+    return " ".join(map(lambda x: "".join([a.lower() if i % 2 else a.upper() for i, a in enumerate(x)]), s.split(" ")))
